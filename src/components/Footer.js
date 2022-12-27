b61503c9a307ef42be6d2d00d68537b6284e0c7b
ciptadii <ciptadi117@gmail.com>
2022-12-27 20:13:13 +0700

    change footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center p-3' style={{ backgroundColor: '#b5b5b5' }}>
-      © 2020 Copyright:
+      © 2022 Copyright:
       <a className='text-white' href='https://app-user-manager.com/'> App User Manager</a>
     </div>
   )
